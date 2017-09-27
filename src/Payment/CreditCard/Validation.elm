0b9c93fa1684e858d85a41bb9cbb1ac9d26a8dfb
Jan Jelinek <kurnick@gmail.com>
2017-09-28 00:27:30 +0200

    Implement validation of longer card numbers and add package.json with elm-test for easier tests running
    resolve #4

--- src/Payment/CreditCard/Validation.elm
+++ src/Payment/CreditCard/Validation.elm
@@ -13,80 +13,83 @@ The main use case is the validation of credit card numbers entered by users in
 an input field. To avoid dependencies to parser libraries there is no
 function with the signature of String -> Bool.
 
+
 # Definition
+
 @docs isValid
 
 -}
 
 import String exposing (toInt, toList, fromList)
 
-{-| Check the given credit card number for validity -}
-isValid : Int -> Bool
-isValid creditCardNumber =
-  if creditCardNumber > 0
-    && (sumDigits <| luhnSecondEach <| toListReverse creditCardNumber ) % 10 == 0 then
-    True
-  else
-    False
 
+{-| Check the given credit card number for validity
+-}
+isValid : String -> Bool
+isValid creditCardNumber =
+    if String.length creditCardNumber > 0 && (sumDigits <| luhnSecondEach <| toListReverse creditCardNumber) % 10 == 0 then
+        True
+    else
+        False
 
 
 luhnSecondEach : List Int -> List Int
 luhnSecondEach list =
-  case list of
-    [] ->
-      []
-    [x] ->
-      [x]
-    x::y::_ ->
-      if 2*y > 9 then
-        x :: sumInt (2*y) :: luhnSecondEach (List.drop 2 list)
-      else
-        x :: (2*y) :: luhnSecondEach (List.drop 2 list)
+    case list of
+        [] ->
+            []
+
+        [ x ] ->
+            [ x ]
+
+        x :: y :: _ ->
+            if 2 * y > 9 then
+                x :: sumInt (2 * y) :: luhnSecondEach (List.drop 2 list)
+            else
+                x :: (2 * y) :: luhnSecondEach (List.drop 2 list)
 
 
-
-toStringRepresentation: Char -> String
+toStringRepresentation : Char -> String
 toStringRepresentation input =
-  fromList [input]
+    fromList [ input ]
 
 
-
-toDigits : Int -> List Int
+toDigits : String -> List Int
 toDigits input =
-  let
-    lastDigit = input % 10
-    toTransform = (input - lastDigit) // 10
+    let
+        lastDigit =
+            input
+                |> String.right 1
+                |> parseInt
 
-    stringDigitsList = List.map toStringRepresentation (toList (toString input))
-  in
-    if input < 0 then
-      []
-    else if input < 10 then
-      [input]
-    else
-      List.map parseInt stringDigitsList
+        stringDigitsList =
+            List.map toStringRepresentation (toList input)
+    in
+        if parseInt input < 0 then
+            []
+        else
+            List.map parseInt stringDigitsList
 
 
-
-toListReverse : Int -> List Int
+toListReverse : String -> List Int
 toListReverse input =
-  List.reverse (toDigits input)
-
+    List.reverse (toDigits input)
 
 
 sumDigits : List Int -> Int
 sumDigits list =
-  List.foldr (+) 0 list
-
+    List.foldr (+) 0 list
 
 
 sumInt : Int -> Int
 sumInt input =
-  sumDigits (toDigits input)
+    sumDigits (toDigits <| toString input)
+
 
 
 -- Simple parseInt helper (since we rely on the Int -> Bool signature for input)
-parseInt: String -> Int
+
+
+parseInt : String -> Int
 parseInt input =
-  Result.withDefault 0 (String.toInt input)
+    Result.withDefault 0 (String.toInt input)
